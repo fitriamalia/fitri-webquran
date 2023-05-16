1eeb96f6186ca2dfbeecba1d443d422bfebc35b5
fitriamalia <fitriamalia200222@gmail.com>
2023-05-16 07:27:57 +0700

    Initial commit

---
+++ src/components/AyatQuran.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="ayat">
+    <li v-for="item in ayatQuran">
+      <p>{{ item.text_uthmani }}</p>
+      <hr>
+    </li>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { ref } from 'vue';
+
+export default {
+
+  data() {
+    return {
+      ayatQuran: ref([])
+    }
+  },
+
+  mounted() {
+    this.getAyatQuran()
+  },
+
+  methods: {
+    getAyatQuran() {
+      axios.get(`https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number=${this.$route.params.id}`)
+          .then ((response) => {
+            console.log(response)
+            this.ayatQuran = response.data.verses
+          }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.ayat {
+  text-align: right;
+  padding-right: 20pt;
+  font-size: 20pt;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="sapa">Al-Quran Indonesia</h1>
+</template>
+
+<style>
+
+.sapa {
+  text-align: center;
+}
+
+</style>
